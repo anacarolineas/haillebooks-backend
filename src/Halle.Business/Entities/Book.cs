fa232d667c74dc98deaf39f39e036cebed2f4dd2
Ana Caroline <anacarolinee.as@gmail.com>
2022-03-20 18:03:00 -0300

    FEAT: entidades

---
+++ src/Halle.Business/Entities/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halle.Business.Entities
+{
+    public class Book : Entity
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [ForeignKey(nameof(Author))]
+        public Guid AuthorId { get; set; }
+        public Author Author { get; set; }
+
+        [ForeignKey(nameof(Category))]
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
