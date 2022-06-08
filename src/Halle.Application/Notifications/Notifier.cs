df1497c18c7d9c4579101050945329eff4e9c365
Ana Caroline <82904559+anacarolineaevo@users.noreply.github.com>
2022-06-07 22:55:19 -0300

    UP: notificador

---
+++ src/Halle.Application/Notifications/Notifier.cs
@@ -0,0 +1,25 @@
+﻿using Halle.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halle.Application.Notifications
+{
+    public class Notifier : INotification
+    {
+        private List<Notification> _notifications = new List<Notification>();
+
+        public List<Notification> GetNotifications() =>
+            _notifications;
+
+        public void Handle(Notification notification) =>
+            _notifications.Add(notification);
+        
+        public bool HasNotifications() =>
+            _notifications.Any();
+
+    }
+   
+}
---
+++ src/Halle.Application/Services/BaseService.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Halle.Application.Interfaces;
+using Halle.Application.Notifications;
+using Halle.Business.Entities;
+
+namespace Halle.Application.Services
+{
+    public abstract class BaseService
+    {
+        private readonly INotification _notification;
+
+        public BaseService(INotification notification) =>
+            _notification = notification;
+
+        protected void Notify(ValidationResult validationResult)
+        {
+            foreach(var error in validationResult.Errors)
+            {
+                Notify(error.ErrorMessage);
+            }
+        }
+
+        protected void Notify(string message) =>
+            _notification.Handle(new Notification(message));
+
+        protected bool ValidateModel<TV, TE>(TV validate, TE model) where TV : AbstractValidator<TE>
+        {
+            var validator = validate.Validate(model);
+
+            if (validator.IsValid) return true;
+
+            Notify(validator);
+
+            return false;
+        }
+    }
+}
