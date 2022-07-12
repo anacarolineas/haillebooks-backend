a673f7fdcfa6d5560bb1c9746393ca829162a1a8
Ana Caroline <82904559+anacarolineaevo@users.noreply.github.com>
2022-07-11 21:49:01 -0300

    NEW: enum tipo livro

---
+++ src/Halle.Application/Enums/EBookType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halle.Application.Enums
+{
+    public enum EBookType
+    {
+        Epub,
+        Pdf,
+        Mobi,
+        Online
+    }
+}
